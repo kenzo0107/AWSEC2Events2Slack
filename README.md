6db1455ea349797e39973308650eedcf8713929f
kenzo.tanaka <kenzo.tanaka@medpeer.co.jp>
2017-07-16 22:22:53 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# AWS EC2 Event Notification to Slack
+
+## Advance preparation
+
+```
+$ pip install lambda-uploader awscli
+$ aws configure --profile <profile>
+```
+
+## File Structure
+
+```
+aws-lambda
+├── README.md
+├── event.json
+├── lambda.json
+├── lambda_function.py
+└── requirements.txt
+```
+
+Main lambda function is `lambda_function.py` with Python 3.6.
+
+- lambda.json
+
+```
+"role": "arn:aws:iam::xxxxxxxxxxxx:role/lambda-check-events-to-slack",
+```
+
+- this `role` is attached a policy for describing ec2 resource.
+
+## Upload to AWS Lambda
+
+```
+$ lambda-uploader --profile <profile>
+
+Î» Building Package
+Î» Uploading Package
+Î» Fin
+```
+
+## Confirm
+![AWS Lambda Console](http://i.imgur.com/G4p85eb.png)
+
+## Save & Test
+
+![Slack](http://i.imgur.com/Eme2uw7.png)
+
+It's good !
+
+THX ♪
---
+++ lambda_function.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+import os
+import slackpy
+import boto3
+import datetime
+
+slack = slackpy.SlackLogger(
+    os.environ['SLACK_INCOMING_WEBHOOK'],
+    os.environ['SLACK_CHANNEL'],
+    os.environ['SLACK_USERNAME'],
+    os.environ['SLACK_ICON_URL']
+)
+slack.set_log_level(slackpy.LogLv.DEBUG)
+
+def lambda_handler(event, context):
+    slack_message =''
+    ec2 = boto3.client('ec2')
+    instance_statuses = ec2.describe_instance_status()['InstanceStatuses']
+    for i in instance_statuses:
+        instance_events = i.get('Events')
+        if instance_events is None:
+            continue
+
+        instance_id = i['InstanceId']
+
+        tags = ec2.describe_instances(
+            InstanceIds=[
+                instance_id
+            ]
+        )['Reservations'][0]['Instances'][0].get('Tags')
+
+        if tags is None:
+            instance_name_tag = 'NoNameTag'
+        else:
+            for j in tags:
+                if j['Key'] == 'Name':
+                    instance_name_tag = j['Value']
+
+        # 通知用メッセージ作成
+        for k in i['Events']:
+            description = k.get('Description')
+            index = description.find("[Completed]")
+            # Description に [Completed] が含まれる場合はスキップ
+            if index != -1:
+                continue
+            not_before = k.get('NotBefore')
+            not_before = not_before + datetime.timedelta(hours=9)
+            not_after  = k.get('NotAfter')
+            not_after  = not_after + datetime.timedelta(hours=9)
+            slack_message = slack_message \
+                + '```' \
+                + '* ' + instance_name_tag + '(' + instance_id + ')\n' \
+                + '[Code] ' + k.get('Code') + '\n' \
+                + '[Description] ' + k.get('Description') + '\n' \
+                + '[NotBefore]   ' + format(not_before.strftime("%Y-%m-%d %H:%M:%S")) + ' (JST)\n' \
+                + '[NotAfter]    ' + format(not_after.strftime("%Y-%m-%d %H:%M:%S")) + ' (JST)\n' \
+                + '``` \n\n'
+
+    if slack_message:
+        slack_message = u'予定されたEC2イベントが見つかりました。以下をご確認ください。\n\n' + slack_message
+        res = slack.error(message=slack_message, title="AWS Scheduled Events Notification")
+    return
